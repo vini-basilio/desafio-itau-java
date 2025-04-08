4328fa37b2b4262c9eb4164d2206ea8edc84b71d
Vini <vini.ba.dev@gmail.com>
2025-04-08 20:55:02 -0300

    feat: implementa métodos para obter e adicionar transações no controller Transacao para debugging

--- vaga/src/main/java/edu/itau/vaga/controllers/Transacao.java
+++ vaga/src/main/java/edu/itau/vaga/controllers/Transacao.java
@@ -3,12 +3,18 @@ package edu.itau.vaga.controllers;
 import edu.itau.vaga.dtos.TransacoDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Transacao {
+    @Autowired
+    private TransacoesHistorico transacoesHistorico;
+
     @GetMapping("/")
+    public List<TransacaoDto> get() {
+        return transacoesHistorico.getTransacoes();
+    }
+
     public String get() {
         return "Hello World";
     }
