0dbd37a62e4939a628297bde5efbd434e1091d23
Vini <vini.ba.dev@gmail.com>
2025-04-08 18:59:20 -0300

    fix: corrige o nome do controller para o padrão pt-br

--- vaga/src/main/java/edu/itau/vaga/controllers/Transacao.java
+++ vaga/src/main/java/edu/itau/vaga/controllers/Transacao.java
@@ -1,12 +1,16 @@
 package edu.itau.vaga.controllers;
 
+import edu.itau.vaga.dtos.TransacoDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Transations {
+public class Transacao {
     @GetMapping("/")
-    public String get(){
+    public String get() {
         return "Hello World";
     }
+
 }
